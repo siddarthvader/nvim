9b458a02e7bf4544f2e188544a4dd5b1cf248d82
siddharth <me@siddharthjain.in>
2024-08-17 17:54:42 +0530

    better

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,30 +1,38 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				filtered_items = {
-					visible = true,
-					show_hidden_count = true,
-					hide_dotfiles = false,
-					hide_gitignored = true,
-					hide_by_name = {
-						-- add extension names you want to explicitly exclude
-						-- '.git',
-						-- '.DS_Store',
-						-- 'thumbs.db',
-					},
-					never_show = {},
-				},
-			},
-		})
-		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>m", ":Neotree close<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- add extension names you want to explicitly exclude
+            '.git',
+            '.DS_Store',
+            'thumbs.db',
+            'node_modules',
+            '.vscode',
+            '.idea',
+          },
+          hide_by_pattern = {
+            -- add pattern names you want to explicitly excludei
+            'ent/**'
+          },
+          never_show = {
+          },
+        },
+      },
+    })
+    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<leader>m", ":Neotree close<CR>", {})
+  end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,14 +5,20 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim","BurntSushi/ripgrep" },
+    dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
     config = function()
       require("telescope").setup({
-        defaults = { 
-         file_ignore_patterns = { 
-            "node_modules",".output",
-  '$houdini','svelte-kit','.build'
-           }
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            ".output",
+            "$houdini",
+            "svelte-kit",
+            ".build",
+            'ent/',
+            'dist/',
+            '**/dist/',
+          },
         },
         extensions = {
           ["ui-select"] = {
@@ -23,7 +29,12 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fs', require('telescope.builtin').current_buffer_fuzzy_find, { desc = 'Search in current buffer' })
+      vim.keymap.set(
+        "n",
+        "<leader>fs",
+        require("telescope.builtin").current_buffer_fuzzy_find,
+        { desc = "Search in current buffer" }
+      )
       require("telescope").load_extension("ui-select")
     end,
   },
