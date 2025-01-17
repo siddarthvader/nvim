7de211a05d06fac996190e4e6c3af421af25c50a
siddharth <me@siddharthjain.in>
2025-01-17 11:05:10 +0530

    using prettierd

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,9 +8,9 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				require("none-ls.diagnostics.eslint_d"),
-			},
+				null_ls.builtins.formatting.prettierd,
+        require("none-ls.diagnostics.eslint"),
+      },
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
