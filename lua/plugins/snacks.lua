87d83f017a970c5b8202b6c0df19a781194aaf87
me@siddharthjain.in <me@siddharthjain.in>
2025-02-04 11:49:32 +0530

    alright

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -78,8 +78,6 @@ return {
     { "gy",              function() Snacks.picker.lsp_type_definitions() end,                    desc = "Goto T[y]pe Definition" },
     { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
     { "<leader>sS",      function() Snacks.picker.lsp_workspace_symbols() end,                   desc = "LSP Workspace Symbols" },
-    { "<leader>z",       function() Snacks.zen() end,                                            desc = "Toggle Zen Mode" },
-    { "<leader>Z",       function() Snacks.zen.zoom() end,                                       desc = "Toggle Zoom" },
     { "<leader>.",       function() Snacks.scratch() end,                                        desc = "Toggle Scratch Buffer" },
     { "<leader>S",       function() Snacks.scratch.select() end,                                 desc = "Select Scratch Buffer" },
     { "<leader>n",       function() Snacks.notifier.show_history() end,                          desc = "Notification History" },
