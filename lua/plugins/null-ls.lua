e87c34c9f2cb180962f899cea1266fac256c8a04
d2du <me@siddharthjain.in>
2025-03-06 08:08:15 +0530

    removed ruff

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,18 +5,8 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort, -- Optional: for import sorting
-        null_ls.builtins.diagnostics.ruff,
       },
       on_attach = function(client, bufnr)
-        -- Auto-formatting on save disabled
-        -- if client.supports_method("textDocument/formatting") then
-        --   vim.api.nvim_clear_autocmds({ buffer = bufnr, group = augroup })
-        --   vim.api.nvim_create_autocmd("BufWritePre", {
-        --     buffer = bufnr,
-        --     callback = function() vim.lsp.buf.format({ bufnr = bufnr }) end,
-        --   })
-        -- end
       end,
     })
   end,
