6f8effc5b79e2701cda5304e0a4f0d41d3a19275
siddharth <me@siddharthjain.in>
2025-03-11 22:42:27 +0530

    fucking hell

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -55,20 +55,6 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
 				}),
 				sources = {
 					{ name = "nvim_lsp", keyword_length = 1, max_item_count = 30 },
