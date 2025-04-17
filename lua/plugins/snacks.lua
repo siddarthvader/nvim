43564f2083c4bcd5a29f377be3e0302ba31cd152
d2du <me@siddharthjain.in>
2025-04-17 08:27:03 +0530

    removed terminal using tmux terminal now

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,25 +1,4 @@
-  return {
-	{
-		"folke/edgy.nvim",
-		---@module 'edgy'
-		---@param opts Edgy.Config
-		opts = function(_, opts)
-			for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
-				opts[pos] = opts[pos] or {}
-				table.insert(opts[pos], {
-					ft = "snacks_terminal",
-					size = { height = 0.4 },
-					title = "%{b:snacks_terminal.id}: %{b:term_title}",
-					filter = function(_buf, win)
-						return vim.w[win].snacks_win
-							and vim.w[win].snacks_win.position == pos
-							and vim.w[win].snacks_win.relative == "editor"
-							and not vim.w[win].trouble_preview
-					end,
-				})
-			end
-		end,
-	},
+return {
 	{
 
 		"folke/snacks.nvim",
@@ -33,42 +12,6 @@
 			input = { enabled = true },
 			notifier = {
 				enabled = true, -- Completely disable Snacks notifications to avoid interference with LSP hover
-			},
-			terminal = {
-				bo = {
-					filetype = "snacks_terminal",
-				},
-				wo = {},
-				keys = {
-					q = "hide",
-					gf = function(self)
-						local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
-						if f == "" then
-							Snacks.notify.warn("No file under cursor")
-						else
-							self:hide()
-							vim.schedule(function()
-								vim.cmd("e " .. f)
-							end)
-						end
-					end,
-					term_normal = {
-						"<esc>",
-						function(self)
-							self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
-							if self.esc_timer:is_active() then
-								self.esc_timer:stop()
-								vim.cmd("stopinsert")
-							else
-								self.esc_timer:start(200, 0, function() end)
-								return "<esc>"
-							end
-						end,
-						mode = "t",
-						expr = true,
-						desc = "Double escape to normal mode",
-					},
-				},
 			},
 			picker = { enabled = true },
 			quickfile = { enabled = true },
@@ -499,20 +442,6 @@
 					Snacks.notifier.hide()
 				end,
 				desc = "Dismiss All Notifications",
-			},
-			{
-				"<leader>tt",
-				function()
-					Snacks.terminal()
-				end,
-				desc = "Toggle Floating Terminal",
-			},
-			{
-				"<c-_>",
-				function()
-					Snacks.terminal()
-				end,
-				desc = "which_key_ignore",
 			},
 			{
 				"]]",
