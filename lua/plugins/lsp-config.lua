f0d78707d71be85bf96cebdef7f962f450bdd23a
d2du <me@siddharthjain.in>
2025-07-14 16:01:42 +0530

    done

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,7 +25,7 @@ return {
       ensure_installed = {
         -- Language Servers only (formatters go in mason.nvim)
         "vtsls",   -- TypeScript/JavaScript
-        "svelte",  -- Svelte
+        "svelte-language-server",  -- Svelte
         "html",    -- HTML
         "cssls",   -- CSS
         "tailwindcss", -- Tailwind CSS
